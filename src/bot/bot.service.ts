34b717de0ca52f47d2b7eb04aeae0fe06f82ed6d
Tuna <df95111111@gmail.com>
2022-12-26 18:56:01 +0800

    feat: 調整為啟動即開啟服務並加入預計開發指令

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BotService {
+  onModuleInit() {
+    this.botMessage();
+  }
   botMessage() {
     process.env.NTBA_FIX_319 = '1';
     const TelegramBot = require('node-telegram-bot-api');
@@ -10,8 +13,23 @@ export class BotService {
 
     const bot = new TelegramBot(token, { polling: true });
 
-    bot.on('message', (msg) => {
-      bot.sendMessage(msg.from.id, msg.text.toString());
+    // Matches /alive
+    bot.onText(/\/alive/, function onDoneText(msg) {
+      bot.sendMessage(msg.from.id, '還活著 一切安好');
+    });
+
+    // Matches /register 將使用者註冊到google sheet
+    bot.onText(/\/register/, function onDoneText(msg) {
+      bot.sendMessage(msg.from.id, 'in register');
+    });
+
+    // Matches /done 紀錄該使用者完成運動到google sheet
+    bot.onText(/\/done/, function onDoneText(msg) {
+      bot.sendMessage(msg.from.id, 'in done');
+    });
+
+    bot.on('inline_query', (msg) => {
+      bot.sendMessage(msg.from.id, 'in inline_query');
     });
   }
 }
